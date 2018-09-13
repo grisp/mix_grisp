24f01b05b18d8659b52275eddbc52b2240533f7d
Adam Lindberg <hello@alind.io>
2018-09-13 13:58:44 +0200

    Add package information to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,26 +1,44 @@
 defmodule MixGrisp.MixProject do
   use Mix.Project
 
-  def project do
+  @source_url "https://github.com/elixir-ecto/postgrex"
+
+  def project() do
     [
       app: :mix_grisp,
       version: "0.1.0",
+      description: "Mix plug-in for GRiSP.",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: @source_url,
     ]
   end
 
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  defp deps do
+  defp deps() do
     [
       {:grisp_tools, "~> 0.2"},
       {:distillery, "~> 2.0"},
     ]
   end
+
+  defp package() do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => "#{@source_url}/blob/master/CHANGELOG",
+      },
+    ]
+  end
+
 end
