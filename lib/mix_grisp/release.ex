256f1c5da5320a95972168f8c24b8fca6b9acb82
Luca Succi <luca.succi@stritzinger.com>
2026-09-04 16:35:59 +0200

    Add workaround to refresh the erts directory on every deployment

--- lib/mix_grisp/release.ex
+++ lib/mix_grisp/release.ex
@@ -10,6 +10,11 @@ defmodule MixGrisp.Release do
 
   def init(release) do
     Process.put(:spec, Map.take(release, [:version, :path, :name]))
+
+    # Mix preserves an existing same-version ERTS directory, which can leave a
+    # stale GRiSP OTP package in the release when the selected package changes.
+    File.rm_rf!(Path.join(release.path, "erts-#{release.erts_version}"))
+
     release
   end
 end
