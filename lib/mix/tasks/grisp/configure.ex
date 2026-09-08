9b2727c443ec6304de213cf97b8475587494b4cf
Luca Succi <luca.succi@stritzinger.com>
2026-09-08 12:25:04 +0200

    Support explicit boolean values in grisp.configure

--- lib/mix/tasks/grisp/configure.ex
+++ lib/mix/tasks/grisp/configure.ex
@@ -7,7 +7,9 @@ defmodule Mix.Tasks.Grisp.Configure do
 
       mix grisp.configure [options]
 
-  Interactive mode is enabled by default. Use `--no-interactive` for scripts.
+  Interactive mode is enabled by default. If the target directory exists,
+  confirmation is required and existing files are preserved. Use
+  `--no-interactive` for scripts.
   """
 
   @switches [
@@ -45,11 +47,53 @@ defmodule Mix.Tasks.Grisp.Configure do
     c: :cookie
   ]
 
+  @boolean_flags %{
+    "--interactive" => "--interactive",
+    "-i" => "--interactive",
+    "--jit" => "--jit",
+    "-j" => "--jit",
+    "--network" => "--network",
+    "-n" => "--network",
+    "--wifi" => "--wifi",
+    "-w" => "--wifi",
+    "--grisp-io" => "--grisp-io",
+    "-g" => "--grisp-io",
+    "--grisp-io-linking" => "--grisp-io-linking",
+    "-l" => "--grisp-io-linking",
+    "--epmd" => "--epmd",
+    "-e" => "--epmd"
+  }
+
   @impl Mix.Task
   def run(args) do
+    args = normalize_boolean_args(args)
     {options, []} = MixGrisp.CLI.parse!(args, @switches, @aliases)
     result = MixGrisp.Configure.run(options)
     Enum.each(result.created, &Mix.shell().info("Created #{&1}"))
     Mix.shell().info("Configured GRiSP Mix project #{result.name}")
   end
+
+  defp normalize_boolean_args([flag, value | rest]) when is_map_key(@boolean_flags, flag) do
+    normalized = Map.fetch!(@boolean_flags, flag)
+
+    case value do
+      "true" ->
+        [normalized | normalize_boolean_args(rest)]
+
+      "false" ->
+        ["--no-" <> String.trim_leading(normalized, "--") | normalize_boolean_args(rest)]
+
+      value when is_binary(value) ->
+        if String.starts_with?(value, "-") do
+          [normalized | normalize_boolean_args([value | rest])]
+        else
+          Mix.raise("Invalid boolean value: #{inspect(value)}")
+        end
+    end
+  end
+
+  defp normalize_boolean_args([argument | rest]),
+    do: [argument | normalize_boolean_args(rest)]
+
+  defp normalize_boolean_args([]), do: []
 end
